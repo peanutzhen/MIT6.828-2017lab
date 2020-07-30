7b63da3340e004b56285dc328c7bafbd18a16c28
astzls213 <astzls213@163.com>
2020-07-30 17:49:42 +0800

    完成Lab3 练习1，同时修改pages映射时的区块大小为PTSIZE，而不是pages数组的大小

--- kern/pmap.c
+++ kern/pmap.c
@@ -166,7 +166,8 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
-
+	envs = (struct Env*) boot_alloc(NENV * sizeof(struct Env));
+	memset(envs, 0, NENV * sizeof(struct Env));
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
 	// up the list of free physical pages. Once we've done so, all further
@@ -191,8 +192,8 @@ mem_init(void)
 	// Your code goes here:
 	// 由于我们要将 VA[UPAGES, UVPT]映射到pages的物理地址空间去
 	// 用boot_map_region是最优雅的实现方式
-	size_t pages_size = ROUNDUP(npages*sizeof(struct PageInfo), PGSIZE); 
-	boot_map_region(kern_pgdir, UPAGES, pages_size, PADDR(pages), PTE_U);
+	//size_t pages_size = ROUNDUP(npages*sizeof(struct PageInfo), PGSIZE); 
+	boot_map_region(kern_pgdir, UPAGES, PTSIZE, PADDR(pages), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map the 'envs' array read-only by the user at linear address UENVS
@@ -201,7 +202,7 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
-
+	boot_map_region(kern_pgdir, UENVS, PTSIZE, PADDR(envs), PTE_U);
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
 	// stack.  The kernel stack grows down from virtual address KSTACKTOP.
