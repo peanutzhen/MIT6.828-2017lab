23c3dfcf5072945f607b4861c3b3401f7d44d6a8
astzls213 <astzls213@163.com>
2020-08-06 11:28:14 +0800

    完成lab3练习5

--- kern/trap.c
+++ kern/trap.c
@@ -195,7 +195,11 @@ trap_dispatch(struct Trapframe *tf)
 {
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
-
+	// 处理 Page Fault
+	if (tf->tf_trapno == T_PGFLT) {
+		page_fault_handler(tf);
+		return;
+	}
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
 	if (tf->tf_cs == GD_KT)
