db78f1e34e79c4f0927302e14d0201049f593ac8
astzls213 <astzls213@163.com>
2020-08-06 15:08:36 +0800

    lab3练习9的补充

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -143,6 +143,8 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 		// Make sure this memory is valid.
 		// Return -1 if it is not.  Hint: Call user_mem_check.
 		// LAB 3: Your code here.
+		if (user_mem_check(curenv, (void *)usd, sizeof(struct UserStabData), PTE_U | PTE_P) != 0)
+			return -1;
 
 		stabs = usd->stabs;
 		stab_end = usd->stab_end;
@@ -151,6 +153,10 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 
 		// Make sure the STABS and string table memory is valid.
 		// LAB 3: Your code here.
+		if (user_mem_check(curenv, stabs, sizeof(struct Stab) * (stab_end - stabs), PTE_U) != 0)
+			return -1;
+		if (user_mem_check(curenv, stabstr, stabstr_end - stabstr, PTE_U) != 0)
+            return -1;
 	}
 
 	// String table validity checks
