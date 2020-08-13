a687ce7aa58f25e2f2990f3d35cb495f81756f23
astzls213 <astzls213@163.com>
2020-08-13 10:29:29 +0800

    Finish lab4 exercise5

--- kern/env.c
+++ kern/env.c
@@ -571,6 +571,9 @@ env_run(struct Env *e)
 	curenv->env_status = ENV_RUNNING;
 	curenv->env_runs++;
 	lcr3(PADDR(curenv->env_pgdir));
+	// release lock of kernel
+	unlock_kernel();
+	// switch to user mode
 	env_pop_tf(&curenv->env_tf);
 	//panic("env_run not yet implemented");
 }
--- kern/trap.c
+++ kern/trap.c
@@ -294,6 +294,8 @@ trap(struct Trapframe *tf)
 		// Acquire the big kernel lock before doing any
 		// serious kernel work.
 		// LAB 4: Your code here.
+		lock_kernel();
+
 		assert(curenv);
 
 		// Garbage collect if current enviroment is a zombie
