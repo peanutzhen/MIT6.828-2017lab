71912e7f7f6a9b32964adc4457337353a1894751
astzls213 <astzls213@163.com>
2020-08-26 15:39:51 +0800

    Lab5 exercise1

--- kern/env.c
+++ kern/env.c
@@ -438,9 +438,12 @@ env_create(uint8_t *binary, enum EnvType type)
 		panic("env_create: %e", rtv);
 	load_icode(e, binary);
 	e->env_type = type;
+
 	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
 	// LAB 5: Your code here.
-	
+	if (type == ENV_TYPE_FS) {
+		e->env_tf.tf_eflags |= FL_IOPL_3;
+	}
 }
 
 //
