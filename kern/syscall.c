b76cbf976c6c73a6c76ebd3750031b3a2955005b
astzls213 <astzls213@163.com>
2020-08-27 21:34:33 +0800

    Lab5 exercise7

--- kern/syscall.c
+++ kern/syscall.c
@@ -150,7 +150,18 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+	int r;
+	struct Env *aEnv;
+	// 获取env
+	if ((r = envid2env(envid, &aEnv, true)) < 0)
+		return r;
+	
+	// 设置trapframe
+	aEnv->env_tf = *tf;
+	aEnv->env_tf.tf_eflags |= FL_IF;
+	aEnv->env_tf.tf_cs |= 3;
+	aEnv->env_tf.tf_eflags &= ~FL_IOPL_MASK;
+	return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -482,6 +493,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 			return sys_ipc_try_send((envid_t) a1, (uint32_t) a2, (void *) a3, (unsigned) a4);
 		case SYS_ipc_recv:
 			return sys_ipc_recv((void *) a1);
+		case SYS_env_set_trapframe:
+			return sys_env_set_trapframe((envid_t) a1, (struct Trapframe *) a2);
 		case NSYSCALLS:
 			return 0;
 		default:
